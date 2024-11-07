c045ed6c7292e1954e6131559d19903e3e9bc78c
garam0107 <name3937@gmail.com>
2024-11-07 17:55:04 +0900

    1107_live

---
+++ 04-component-state-flow/vue-project/src/components/Parent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ParentChild 
+    my-msg = "message"
+    :dynamic-props = "name"
+    @some-event = "someCallback"
+    @emit-args = "receiveCallback"
+    @update-name = "updateName"
+    />
+    <ParentItem 
+    v-for = "item in items"
+    :key = "item.id"
+    :my-prop = "item"
+    />
+  </div>
+</template>
+
+<script setup>
+import ParentChild from '@/components/ParentChild.vue'
+import ParentItem from '@/components/ParentItem.vue'
+import { ref } from 'vue'
+
+const name = ref('Garam')
+const items = ref([
+  { id: 1, name: '사과'},
+  { id: 2, name: '딸기' },
+  { id: 3, name: '바나나'}
+
+])
+
+const someCallback = () => {
+  console.log('자식 이벤트 수신')
+}
+const receiveCallback = (...args) => {
+  console.log(args)
+  console.log(`추가인자 ${args} 수신`)
+}
+const updateName = () => {
+  name.value = "이름 변경"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 04-component-state-flow/vue-project/src/components/ParentChild.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>{{myMsg}}</p>
+    <p>{{dynamicProps}}</p>
+    <ParentGrandChild 
+    :my-msg = "myMsg"
+    @update-name="updateName"
+    />
+    <div>
+      <button @click = "buttonClick">클릭</button>
+      <button @click = "serveArgs">추가 인자 전달</button>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import ParentGrandChild from '@/components/ParentGrandChild.vue'
+
+defineProps({
+  myMsg: String,
+  dynamicProps: String,
+})
+// emit 이벤트 선언
+const emit = defineEmits(['someEvent', 'emitArgs', 'updateName'])
+const buttonClick = () => {
+  emit('someEvent')
+}
+const serveArgs = () => {
+  emit('emitArgs', 1,2,3)
+}
+const updateName = () => {
+  emit('updateName')
+}
+</script>
+
+<style scoped>
+
+</style>
